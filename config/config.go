361f02eb4d22494915c036f3b4d01083623c03b2
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-05-04 21:53:12 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.178.0 (#45)

--- config/config.go
+++ config/config.go
@@ -1,10 +1,22 @@
 package config
 
 import (
+	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/config"
 )
+
+// NewConfig for config.
+func NewConfig(i *cmd.InputConfig) (*Config, error) {
+	c := &Config{}
+
+	return c, i.Unmarshal(c)
+}
 
 // Config for the client.
 type Config struct {
 	*config.Config `yaml:",inline" json:",inline" toml:",inline"`
+}
+
+func decorateConfig(cfg *Config) *config.Config {
+	return cfg.Config
 }
