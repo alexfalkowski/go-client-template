b78e6e99d49f46986b50e001896118f4336ab830
Alex Falkowski <alexrfalkowski@gmail.com>
2024-05-13 13:23:38 +0200

    fix(config): guard for missing config

--- config/config.go
+++ config/config.go
@@ -12,11 +12,20 @@ func NewConfig(i *cmd.InputConfig) (*Config, error) {
 	return c, i.Unmarshal(c)
 }
 
+// IsEnabled for config.
+func IsEnabled(cfg *Config) bool {
+	return cfg != nil
+}
+
 // Config for the client.
 type Config struct {
 	*config.Config `yaml:",inline" json:",inline" toml:",inline"`
 }
 
 func decorateConfig(cfg *Config) *config.Config {
+	if !IsEnabled(cfg) {
+		return nil
+	}
+
 	return cfg.Config
 }
