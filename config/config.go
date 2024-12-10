657942731b8d435ab4d341dc6acf2140a1578e7e
Alex Falkowski <alexrfalkowski@gmail.com>
2024-12-10 23:27:58 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.329.0
    https://github.com/alexfalkowski/go-service/pull/1064

--- config/config.go
+++ config/config.go
@@ -1,31 +1,23 @@
 package config
 
 import (
-	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/config"
 )
-
-// NewConfig for config.
-func NewConfig(i *cmd.InputConfig) (*Config, error) {
-	c := &Config{}
-
-	return c, i.Decode(c)
-}
-
-// IsEnabled for config.
-func IsEnabled(cfg *Config) bool {
-	return cfg != nil
-}
 
 // Config for the client.
 type Config struct {
 	*config.Config `yaml:",inline" json:",inline" toml:",inline"`
 }
 
-func decorateConfig(cfg *Config) *config.Config {
-	if !IsEnabled(cfg) {
-		return nil
+// Valid or error.
+func (c Config) Valid() error {
+	if c.Config == nil {
+		return config.ErrInvalidConfig
 	}
 
+	return c.Config.Valid()
+}
+
+func decorateConfig(cfg *Config) *config.Config {
 	return cfg.Config
 }
