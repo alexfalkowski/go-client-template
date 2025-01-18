ed53e5869ec87a85cfb875ca457c75ab194c6236
Alex Falkowski <alexrfalkowski@gmail.com>
2025-01-18 08:59:51 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.376.0
    https://github.com/alexfalkowski/go-service/pull/1194

--- cmd/client.go
+++ cmd/client.go
@@ -3,17 +3,14 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-client-template/cmd/client"
 	"github.com/alexfalkowski/go-client-template/config"
-	"github.com/alexfalkowski/go-service/compress"
-	"github.com/alexfalkowski/go-service/encoding"
 	"github.com/alexfalkowski/go-service/feature"
-	"github.com/alexfalkowski/go-service/sync"
+	"github.com/alexfalkowski/go-service/module"
 	"github.com/alexfalkowski/go-service/telemetry"
 	"go.uber.org/fx"
 )
 
 // ClientOptions for cmd.
 var ClientOptions = []fx.Option{
-	sync.Module, compress.Module, encoding.Module,
-	feature.Module, telemetry.Module,
+	module.Module, feature.Module, telemetry.Module,
 	config.Module, client.Module, Module,
 }
