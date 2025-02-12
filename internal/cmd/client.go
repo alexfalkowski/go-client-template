8b4af732f70d1f5e87b14d1c190284c12b4b78d0
Alex Falkowski <alexrfalkowski@gmail.com>
2025-02-12 17:46:47 +0100

    feat(cmd): move to use register

--- internal/cmd/client.go
+++ internal/cmd/client.go
@@ -3,14 +3,19 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-client-template/internal/cmd/client"
 	"github.com/alexfalkowski/go-client-template/internal/config"
+	"github.com/alexfalkowski/go-service/cmd"
 	"github.com/alexfalkowski/go-service/feature"
 	"github.com/alexfalkowski/go-service/module"
 	"github.com/alexfalkowski/go-service/telemetry"
-	"go.uber.org/fx"
 )
 
-// ClientOptions for cmd.
-var ClientOptions = []fx.Option{
-	module.Module, feature.Module, telemetry.Module,
-	config.Module, client.Module, Module,
+// RegisterClient for cmd.
+func RegisterClient(command *cmd.Command) {
+	client := command.AddClient("client", "Start client",
+		module.Module, feature.Module, telemetry.Module,
+		config.Module, client.Module, Module,
+	)
+
+	command.RegisterInput(client, "")
+	command.RegisterOutput(client, "")
 }
