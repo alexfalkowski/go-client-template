7ed65be68b232ed628d2f34bb685505aa4c09eeb
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-02-15 11:06:57 +0100

    feat(deps): update github.com/alexfalkowski/go-service to v1.434.0 (#438)
    https://github.com/alexfalkowski/go-service/pull/1307

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/alexfalkowski/go-client-template/internal/cmd"
 	sc "github.com/alexfalkowski/go-service/cmd"
+	"github.com/alexfalkowski/go-service/env"
 )
 
 func main() {
@@ -10,7 +11,7 @@ func main() {
 }
 
 func command() *sc.Command {
-	command := sc.New(cmd.Version)
+	command := sc.New(env.NewVersion().String())
 
 	cmd.RegisterClient(command)
 
