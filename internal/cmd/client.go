f0a09f00d1d300e6134156780fed8ad23c455c35
Alex Falkowski <alexrfalkowski@gmail.com>
2025-05-28 15:55:36 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.19.0
    https://github.com/alexfalkowski/go-service/pull/1642

--- internal/cmd/client.go
+++ internal/cmd/client.go
@@ -2,18 +2,12 @@ package cmd
 
 import (
 	"github.com/alexfalkowski/go-client-template/internal/cmd/client"
-	"github.com/alexfalkowski/go-client-template/internal/config"
 	"github.com/alexfalkowski/go-service/v2/cli"
-	"github.com/alexfalkowski/go-service/v2/feature"
-	"github.com/alexfalkowski/go-service/v2/module"
-	"github.com/alexfalkowski/go-service/v2/telemetry"
 )
 
 // RegisterClient for cmd.
 func RegisterClient(command cli.Commander) {
-	cmd := command.AddClient("client", "Start client",
-		module.Module, feature.Module, telemetry.Module,
-		config.Module, client.Module, cli.Module,
-	)
+	cmd := command.AddClient("client", "Start client", client.Module)
+
 	cmd.AddInput("")
 }
--- internal/cmd/client/module.go
+++ internal/cmd/client/module.go
@@ -1,10 +1,14 @@
 package client
 
 import (
+	"github.com/alexfalkowski/go-client-template/internal/config"
+	"github.com/alexfalkowski/go-service/v2/module"
 	"go.uber.org/fx"
 )
 
 // Module for fx.
 var Module = fx.Options(
+	module.Client,
+	config.Module,
 	fx.Invoke(Start),
 )
