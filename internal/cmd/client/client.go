6fb51a2b327b1fd5f7e618c61a7a28e4f32026f4
Alex Falkowski <alexrfalkowski@gmail.com>
2025-06-10 20:02:00 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.65.0
    https://github.com/alexfalkowski/go-service/pull/1716

--- internal/cmd/client/client.go
+++ internal/cmd/client/client.go
@@ -3,21 +3,21 @@ package client
 import (
 	"context"
 
+	"github.com/alexfalkowski/go-service/v2/di"
 	"github.com/alexfalkowski/go-service/v2/telemetry/logger"
-	"go.uber.org/fx"
 )
 
 // Params for config.
 type Params struct {
-	fx.In
+	di.In
 
-	Lifecycle fx.Lifecycle
+	Lifecycle di.Lifecycle
 	Logger    *logger.Logger
 }
 
 // Start the client.
 func Start(params Params) {
-	params.Lifecycle.Append(fx.Hook{
+	params.Lifecycle.Append(di.Hook{
 		OnStart: func(ctx context.Context) error {
 			params.Logger.Log(ctx, logger.NewText("awesome client"))
 
