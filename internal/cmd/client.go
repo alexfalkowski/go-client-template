8e269ab719fcabb6dcc234ef0774188035d907a0
Alex Falkowski <alexrfalkowski@gmail.com>
2025-11-24 20:51:24 +0100

    feat(strings): use strings.Empty

--- internal/cmd/client.go
+++ internal/cmd/client.go
@@ -3,11 +3,11 @@ package cmd
 import (
 	"github.com/alexfalkowski/go-client-template/internal/cmd/client"
 	"github.com/alexfalkowski/go-service/v2/cli"
+	"github.com/alexfalkowski/go-service/v2/strings"
 )
 
 // RegisterClient for cmd.
 func RegisterClient(command cli.Commander) {
 	cmd := command.AddClient("client", "Start client", client.Module)
-
-	cmd.AddInput("")
+	cmd.AddInput(strings.Empty)
 }
