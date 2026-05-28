ac19b05ad579d17b574423536db4216f2f6fac95
Alex Falkowski <alexrfalkowski@gmail.com>
2026-05-28 22:50:35 +0200

    feat(config): make it required

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,10 @@
 package config
 
-import (
-	"github.com/alexfalkowski/go-service/v2/config"
-)
+import "github.com/alexfalkowski/go-service/v2/config"
 
 // Config for the client.
 type Config struct {
-	*config.Config `yaml:",inline" json:",inline" toml:",inline"`
+	*config.Config `yaml:",inline" json:",inline" toml:",inline" validate:"required"`
 }
 
 func decorateConfig(cfg *Config) *config.Config {
